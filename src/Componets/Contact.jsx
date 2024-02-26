0b236ac64d5f26f73ea363e6e673d5a47f0f3dca
Tushar12152 <tusharimran470@gmail.com>
2024-02-26 21:59:18 +0600

    email js implemented

--- src/Componets/Contact.jsx
+++ src/Componets/Contact.jsx
@@ -1,13 +1,24 @@
-import swal from "sweetalert";
+
 import Title from "../Shared/Title";
 import Container from "../Shared/Container";
-
+import emailjs from '@emailjs/browser';
+import { useRef } from "react";
+import swal from "sweetalert";
 
 const Contact = () => {
 
+  const form = useRef();
+
     const handleSend=e=>{
          e.preventDefault()
-         swal("success","Your message is successFully send to Admin panel","success")
+         emailjs.sendForm('service_ad7k4vx', 'template_wzvxuyn', form.current, 'Ne7PlQpzjlHbaRdnK')
+         .then((result) => {
+             if(result.text){
+              swal('Thank you for your message')
+             }
+         }, (error) => {
+             console.log(error.text);
+         });
     }
 
 
@@ -19,13 +30,13 @@ const Contact = () => {
    
    </div>
 
-        <form onSubmit={handleSend}>
+        <form  ref={form} onSubmit={handleSend}>
     <div className="flex w-full   gap-2">
         <div className="form-control w-[50%]">
           <label className="label">
             <span className="label-text text-white">First Name</span>
           </label>
-          <input  type="text" placeholder="First Name" className="input border-2 border-[#f8f83ce1] w-full" required />
+          <input name="user_name"  type="text" placeholder="First Name" className="input border-2 border-[#f8f83ce1] w-full" required />
         </div>
 
         <div className="form-control w-[50%]">
@@ -48,19 +59,19 @@ const Contact = () => {
           <label className="label">
             <span className="label-text text-white">Email</span>
           </label>
-          <input name="description" type="text" placeholder="Email" className="input border-2 border-[#f8f83ce1] w-full " required />
+          <input name="user_email" type="text" placeholder="Email" className="input border-2 border-[#f8f83ce1] w-full " required />
         </div>
 
         <div className="form-control w-[50%]">
           <label className="label">
             <span className="label-text text-white">Phone</span>
           </label>
-          <input name="description" type="text" placeholder="Phone" className="input border-2 border-[#f8f83ce1] w-full " required />
+          <input name="user_Phone" type="text" placeholder="Phone" className="input border-2 border-[#f8f83ce1] w-full " required />
         </div>
 
     </div>
 
-   <textarea  placeholder="Write Your Text.................." className="border-2 border-[#f8f83ce1] w-full mt-6 rounded-lg p-4" name="" id="" cols="30" rows="3"></textarea>
+   <textarea  placeholder="Write Your Text.................." className="border-2 border-[#f8f83ce1] w-full mt-6 rounded-lg p-4" name="message" id="" cols="30" rows="3"></textarea>
    
 
        <input className="btn w-full mt-5 bg-[#f8f83ce1] text-black" type="submit" value="Send" />
